a7b31ec028fa4a5a0133f2a3a017cfc981ee4e0e
Michael Stoelting <ms@reflect-media.de>
2021-07-09 08:34:49 +0200

    udemy course symfony - show menu card

--- symfony_webproject/app/src/Controller/HomeController.php
+++ symfony_webproject/app/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GerichtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(GerichtRepository $gr): Response
     {
+        $gerichte = $gr->findAll();
+
+        $zufall = array_rand($gerichte, 2);
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'gericht1' => $gerichte[$zufall[0]],
+            'gericht2' => $gerichte[$zufall[1]]
         ]);
     }
 }
--- symfony_webproject/app/src/Controller/MenuController.php
+++ symfony_webproject/app/src/Controller/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GerichtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'menu')]
-    public function index(): Response
+    public function menu(GerichtRepository $gr): Response
     {
+        $gericht = $gr->findAll();
+
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+            'gerichte' => $gericht,
         ]);
     }
 }
