f5b2de2e352de135cad080de26283e8dcddbb15b
Michael Stoelting <ms@reflect-media.de>
2021-09-08 08:45:17 +0200

    add sessions table

--- symfony_book_2/app/guestbook/migrations/Version20210904200618.php
+++ symfony_book_2/app/guestbook/migrations/Version20210904200618.php
@@ -22,6 +22,15 @@ final class Version20210904200618 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, conference_id INT NOT NULL, author VARCHAR(255) NOT NULL, text LONGTEXT NOT NULL, email VARCHAR(255) NOT NULL, create_at DATETIME NOT NULL, INDEX IDX_9474526C604B8382 (conference_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C604B8382 FOREIGN KEY (conference_id) REFERENCES conference (id)');
+
+        $this->addSql('
+            CREATE TABLE sessions (
+                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                sess_data BLOB NOT NULL,
+                sess_lifetime INTEGER NOT NULL,
+                sess_time INTEGER NOT NULL
+            )
+        ');
     }
 
     public function down(Schema $schema): void
